269bfc8340202c055e27c1cb00f8168dc00d01f7
anantwaghblenheim <anant.wagh@blenheimchalcot.com>
2024-06-01 10:35:42 +0530

    Run and publish unit tests 6.2

--- Tailspin.SpaceGame.Web/IDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/IDocumentDBRepository.cs
@@ -1,7 +1,7 @@
 ﻿[TestCase(0, ExpectedResult=0)]
 [TestCase(1, ExpectedResult=1)]
 [TestCase(10, ExpectedResult=10)]
-public int ReturnRequestedCount(int count)
+private int ReturnRequestedCount(int count)
 {
     const int PAGE = 0; // take the first page of results
 
